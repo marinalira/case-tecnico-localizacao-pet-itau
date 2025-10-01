b949d6951cda0e8c690b44d16c156da89b35899e
marinalira <marina.silva@onebox.one>
2025-09-30 23:31:18 -0300

    main

--- src/main/java/br/com/itaucase/localizacao_pet/LocalizacaoPetApplication.java
+++ src/main/java/br/com/itaucase/localizacao_pet/LocalizacaoPetApplication.java
@@ -2,8 +2,10 @@ package br.com.itaucase.localizacao_pet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LocalizacaoPetApplication {
 
 	public static void main(String[] args) {
